e5e33db62ac7596fb510064361b47014023c609d
ngphctn14 <ngophuctan1407@gmail.com>
2025-04-14 16:21:30 +0700

    Setup

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : Monster
+{
+    [SerializeField]
+    private int health;
+
+    [SerializeField]
+    private int speed;
+
+    public Ghost(int health, int speed) 
+    {
+        this.health = health;
+        this.speed = speed;
+    }
+    private void Update()
+    {
+        FollowPlayer();
+    }
+
+    public void FollowPlayer()
+    {
+        Vector3 playerPosition = Player.Instance.transform.position;
+        playerPosition.y = transform.position.y;
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed * Time.deltaTime);
+    }
+
+    public override Monster Clone()
+    {
+        Monster monster = null;
+
+        monster = Instantiate(this, transform.localPosition, transform.localRotation);
+
+        monster.transform.localScale = transform.localScale;
+
+        monster.gameObject.SetActive(true);
+
+        return monster.GetComponent<Ghost>();
+    }
+}
